9e439c0995f6d82c73d05677bda0515c85151524
Roman <31464179+grand-roman@users.noreply.github.com>
2023-03-09 14:22:31 +0700

    Update SingleViewController.swift

--- ImageFeed/SingleImage/SingleViewController.swift
+++ ImageFeed/SingleImage/SingleViewController.swift
@@ -1,4 +1,3 @@
-
 import UIKit
 
 final class SingleViewController: UIViewController {
@@ -6,21 +5,14 @@ final class SingleViewController: UIViewController {
     @IBOutlet private weak var scrollView: UIScrollView!
     @IBOutlet private weak var fullImageView: UIImageView!
     
-    var image: UIImage! {
-        didSet {
-            guard isViewLoaded else { return }
-            fullImageView.image = image
-            rescaleAndCenterImageInScrollView(image: image)
-        }
-    }
+    var image: URL!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         scrollView.minimumZoomScale = 0.1
         scrollView.maximumZoomScale = 1.25
-        fullImageView.image = image
-        rescaleAndCenterImageInScrollView(image: image)
+        setImage()
     }
     
     @IBAction private func didTapBackButton() {
@@ -31,6 +23,41 @@ final class SingleViewController: UIViewController {
         let items = [image]
         let activityVC = UIActivityViewController(activityItems: items as [Any], applicationActivities: nil)
         present(activityVC, animated: true)
+    }
+    func showError() {
+        let alert = UIAlertController(
+            title: "Ошибка",
+            message: "Что-то пошло не так. Попробовать еще раз?",
+            preferredStyle: .alert)
+        let nopeButton = UIAlertAction(
+            title: "Не надо",
+            style: .default) { [weak self] _ in
+                self?.dismiss(animated: true)
+            }
+        let againButton = UIAlertAction(
+            title: "Повторить",
+            style: .default) { [weak self] _ in
+                self?.setImage()
+            }
+        alert.addAction(nopeButton)
+        alert.addAction(againButton)
+        present(alert, animated: true)
+    }
+    
+    func setImage() {
+        UIBlockingProgressHUD.show()
+        fullImageView.kf.setImage(with: image) { [weak self] result in
+            UIBlockingProgressHUD.dismiss()
+            guard let self else { return }
+            
+            switch result {
+            case .success(let imageModel):
+                self.rescaleAndCenterImageInScrollView(image: imageModel.image)
+            case .failure(let error):
+                print(error)
+                self.showError()
+            }
+        }
     }
     
     private func rescaleAndCenterImageInScrollView(image: UIImage) {
